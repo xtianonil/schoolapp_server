9584f6e6c66b53b4faceb1acfe13e16d4f1f33b7
xtianonil <co.tolentino@gmail.com>
2016-11-29 21:00:07 +0800

    #

---
+++ user_list.php
@@ -0,0 +1,16 @@
+<?php
+	include "db.php";
+	$sql = "select * from `user` where user_type NOT LIKE 'school_admin'";
+		$select = mysqli_query($con,$sql);
+		$numrows = mysqli_num_rows($select);
+		if ($numrows > 0)
+		{
+			$rows = array();
+			while($row=mysqli_fetch_array($select))
+				$rows[]= $row;
+			echo json_encode($rows);
+		}
+		else
+			echo "login_error";
+		mysqli_close($con);
+?>
